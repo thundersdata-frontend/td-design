ac7525cc2cc71c3246d5c612c5c017972397604c
liaojun <767882503@qq.com>
2019-10-23 13:39:30 +0800

    refactor: utils移除commit等配置
    1、移除commit类配置；
    2、移除eslint与prettier类配置；
    3、tsconfig从根目录继承。

--- packages/utils/release.js
+++
@@ -1,9 +0,0 @@
-const standardVersion = require('standard-version');
-
-standardVersion({
-  noVerify: true,
-  infile: 'CHANGELOG.md',
-  silent: true,
-  preset: 'custom-config',
-  sameFile: true,
-});
