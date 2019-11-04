1b1962929f1050314287ffd4bc152c4a4a398b00
qcc <quchaochao@thundersdata.com>
2019-11-04 17:59:53 +0800

    feat: commit修改

--- packages/web/__tests__/SMSInput/index.test.tsx
+++ packages/web/__tests__/SMSInput/index.test.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow ,mount} from 'enzyme';
-import CheckCode from '../../components/sms-input';
+import SMSInput from '../../components/sms-input';
 
 describe('测试CheckCode组件', () => {
   it('1. 成功渲染', () => {
-    const wrapper = shallow(<CheckCode type={1}/>);
+    const wrapper = shallow(<SMSInput type={1}/>);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('2. 测试一开始的展示文字', () => {
-    const wrapper = mount(<CheckCode type={1} phone='123456789'/>);
+    const wrapper = mount(<SMSInput type={1} phone='123456789'/>);
     expect( wrapper.find('a').text()).toBe('获取验证码')
   });
   
