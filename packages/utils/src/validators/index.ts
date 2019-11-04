39f9b54b2a1d832204969a3af51b791651817785
hss <huangshanshan@thundersdata.onaliyun.com>
2019-11-04 10:51:07 +0800

    fix: 修复validators文件导出方式不正确的问题

--- packages/utils/src/validators/index.ts
+++ packages/utils/src/validators/index.ts
@@ -3,21 +3,23 @@
  * @公司: thundersdata
  * @作者: 瞿超超
  * @Date: 2019-10-29 16:17:00
- * @LastEditors: 陈杰
- * @LastEditTime: 2019-11-01 10:53:39
+ * @LastEditors: 黄姗姗
+ * @LastEditTime: 2019-11-04 10:47:04
  */
 import reg from '../regex/index';
 
-/**
- * 手机号码校验
- * @param _rule
- * @param value
- * @param callback
- */
-export function phoneValidator(_: unknown, value: string, callback: (message?: string) => void) {
-  if (value && !reg.isPhone(value)) {
-    callback('请输入有效的电话号码');
-  } else {
-    callback();
+export default {
+  /**
+   * 手机号码校验
+   * @param _rule
+   * @param value
+   * @param callback
+   */
+  phoneValidator(_: unknown, value: string, callback: (message?: string) => void) {
+    if (value && !reg.isPhone(value)) {
+      callback('请输入有效的电话号码');
+    } else {
+      callback();
+    }
   }
 }
