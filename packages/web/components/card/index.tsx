bbb3f874d6ddac3ae34d7b1455f2d10511fd63b6
chj_damon <chjdamon@gmail.com>
2019-11-13 14:30:26 +0800

    fix: 修复Card组件无法使用ref的问题

--- packages/web/components/card/index.tsx
+++ packages/web/components/card/index.tsx
@@ -4,7 +4,7 @@
  * @作者: 廖军
  * @Date: 2019-07-09 17:19:00
  * @LastEditors: 陈杰
- * @LastEditTime: 2019-11-13 13:51:37
+ * @LastEditTime: 2019-11-13 14:28:28
  */
 import React, { CSSProperties } from 'react';
 import classnames from 'classnames';
@@ -17,7 +17,7 @@ export interface CardProps {
   style?: CSSProperties;
 }
 
-const Card: React.FC<CardProps> = React.forwardRef<HTMLDivElement, CardProps>(({ title, subtitle, extra, className, style, children }, ref) => {
+const Card = React.forwardRef<HTMLDivElement, CardProps>(({ title, subtitle, extra, className, style, children }, ref) => {
   const SubTitleComp = subtitle ? <div className="td-card-header-subtitle">{subtitle}</div> : null;
 
   // title
