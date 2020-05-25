fe79733274adf825d8d09aed8ac98f5a8d61a419
rxs-michael <ruanxusong@thundersdata.com>
2020-05-25 14:16:52 +0800

    fix: 修改ComBlock主题样式

--- packages/charts/src/components/com-block/index.tsx
+++ packages/charts/src/components/com-block/index.tsx
@@ -4,10 +4,11 @@
  * @作者: 廖军
  * @Date: 2019-09-09 13:50:09
  * @LastEditors: 阮旭松
- * @LastEditTime: 2020-05-15 14:27:18
+ * @LastEditTime: 2020-05-25 14:11:15
  */
 import React, { CSSProperties } from 'react';
 import classnames from 'classnames';
+import { theme } from '../../config';
 
 export interface ComBlockProps {
   className?: string;
@@ -20,11 +21,23 @@ const ComBlock: React.FC<ComBlockProps> = props => {
   const { className, style = {}, contentClassName, onClick } = props;
 
   return (
-    <div style={style} className={classnames('td-chart-comBlock', className)} onClick={onClick}>
-      <div className="lt-border" />
-      <div className="lr-border" />
-      <div className="lb-border" />
-      <div className="rb-border" />
+    <div
+      style={style}
+      className={classnames(
+        'td-chart-comBlock',
+        theme !== 'dark' ? 'td-chart-noBordered' : '',
+        className,
+      )}
+      onClick={onClick}
+    >
+      {theme === 'dark' && (
+        <>
+          <div className="lt-border" />
+          <div className="lr-border" />
+          <div className="lb-border" />
+          <div className="rb-border" />
+        </>
+      )}
       <div className={classnames('td-chart-content', contentClassName)}>{props.children}</div>
     </div>
   );
