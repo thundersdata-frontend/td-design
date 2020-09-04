774a82e5ea27fd2d35c3f95c518c358d3580f682
chj_damon <chjdamon@gmail.com>
2020-09-04 18:57:05 +0800

    feat: 新增Flex/WhiteSpace/WingBlank三个组件
    1. 导出Spacing类型
    2. 补充项目中自定义主题效果

---
+++ packages/react-native/components/flex/FlexItem.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import { View } from 'react-native';
+import {
+  spacing,
+  SpacingProps,
+  border,
+  BorderProps,
+  layout,
+  LayoutProps,
+  useRestyle,
+} from '@shopify/restyle';
+import { Theme } from '../config/theme';
+
+const restyleFunctions = [spacing, border, layout];
+
+type FlexItemProps = SpacingProps<Theme> & BorderProps<Theme> & Omit<LayoutProps<Theme>, 'width'>;
+
+const FlexItem: FC<FlexItemProps> = ({ children, ...restProps }) => {
+  const props = useRestyle(restyleFunctions, {
+    flex: 1,
+    justifyContent: 'center',
+    ...restProps,
+  });
+  return <View {...props}>{children}</View>;
+};
+
+export default FlexItem;
--- packages/react-native/example/src/App.tsx
+++ packages/react-native/example/src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { ThemeProvider, Box, Text } from '@td-design/react-native';
+import { ThemeProvider, Flex, Text, Box, WhiteSpace, WingBlank } from '@td-design/react-native';
+import { theme } from './theme';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <ThemeProvider>
-        <Box>
-          <Text variant="body">123</Text>
+      <ThemeProvider {...{ theme }}>
+        <Flex height={200} borderWidth={1} backgroundColor="mainBackground">
+          <Flex.Item borderColor="borderColor" borderWidth={1} marginRight="s">
+            <Text variant="body">123</Text>
+          </Flex.Item>
+          <Flex.Item>
+            <Box borderColor="borderColor" borderWidth={1}>
+              <Text variant="body">456</Text>
+            </Box>
+          </Flex.Item>
+        </Flex>
+        <WhiteSpace size="xxl" />
+        <Box borderColor="borderColor" borderWidth={1}>
+          <Text>789</Text>
         </Box>
+        <WhiteSpace />
+        <WingBlank size="xl">
+          <Box borderColor="borderColor" borderWidth={1}>
+            <Text>000</Text>
+          </Box>
+        </WingBlank>
       </ThemeProvider>
     </SafeAreaView>
   );
