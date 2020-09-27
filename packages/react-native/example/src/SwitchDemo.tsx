92925dae2e1f86aa00a8095bfdbd5ad88e8a7260
chenyingjie <13487079308@163.com>
2020-09-27 19:05:25 +0800

    frat:switch组件

--- packages/react-native/example/src/SwitchDemo.tsx
+++ packages/react-native/example/src/SwitchDemo.tsx
@@ -1,23 +1,50 @@
 import React, { useState } from 'react';
-import { Button, Switch } from '@td-design/react-native';
+import { Button, Icon, Switch, WhiteSpace } from '@td-design/react-native';
 import { View } from 'react-native';
+import Iconfont from './Iconfont';
 
 export default () => {
   const [checked, setChecked] = useState<boolean>(false);
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <WhiteSpace></WhiteSpace>
       <Switch
         checked={checked}
         onChange={checked => {
-          console.log(checked, 'checked');
           setChecked(checked);
         }}
       />
+      <WhiteSpace></WhiteSpace>
       <Switch
         checked={checked}
-        disabled={true}
+        disabled
         onChange={checked => {
-          console.log(checked, 'checked');
+          setChecked(checked);
+        }}
+      />
+      <WhiteSpace></WhiteSpace>
+      <Switch
+        checked={checked}
+        color="#875467"
+        onChange={checked => {
+          setChecked(checked);
+        }}
+      />
+      <WhiteSpace></WhiteSpace>
+      <Switch
+        checked={checked}
+        circleActive="开"
+        circleInactive="关"
+        onChange={checked => {
+          setChecked(checked);
+        }}
+      />
+      <WhiteSpace></WhiteSpace>
+      <Switch
+        checked={checked}
+        circleActive={<Iconfont name="icon_selected" size={24}></Iconfont>}
+        circleInactive={<Iconfont name="icon_close"></Iconfont>}
+        onChange={checked => {
           setChecked(checked);
         }}
       />
