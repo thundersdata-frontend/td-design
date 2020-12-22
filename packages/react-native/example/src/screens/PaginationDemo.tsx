5fae284b4e9b8b6e232ce1ccadd83e619b535527
chenyingjie <13487079308@163.com>
2020-12-22 15:28:20 +0800

    refactor: 修改demo

--- packages/react-native/example/src/screens/PaginationDemo.tsx
+++ packages/react-native/example/src/screens/PaginationDemo.tsx
@@ -6,14 +6,14 @@ export default () => {
   return (
     <ScrollView style={{ flex: 1 }}>
       <WingBlank>
-        <Text>常规:</Text>
+        {/* <Text>常规:</Text>
         <Pagination
           total={66}
           onChange={e => {
             Alert.alert(e + '');
           }}
-        />
-        <WhiteSpace />
+        /> */}
+        {/* <WhiteSpace />
         <Text>设置page:</Text>
         <Pagination
           page={3}
@@ -21,7 +21,7 @@ export default () => {
           onChange={e => {
             Alert.alert(e + '');
           }}
-        />
+        /> */}
         <WhiteSpace />
         <Text>自定义render:</Text>
         <Pagination
@@ -30,10 +30,10 @@ export default () => {
             Alert.alert(e + '');
           }}
           prevButtonRender={isFirstPage => {
-            return isFirstPage ? <Text>{isFirstPage}</Text> : <Text>notFirstPage</Text>;
+            return isFirstPage ? <Text>isFirstPage</Text> : <Text>notFirstPage</Text>;
           }}
           nextButtonRender={isLastPage => {
-            return <>{isLastPage ? <Text>LastPage</Text> : <Text>notLastPage</Text>}</>;
+            return isLastPage ? <Text>LastPage</Text> : <Text>notLastPage</Text>;
           }}
           counterRender={(currentindex, totalPages) => {
             return <Text>{currentindex + '/' + totalPages}</Text>;
