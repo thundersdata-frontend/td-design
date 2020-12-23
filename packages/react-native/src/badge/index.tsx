e81739fa8a396ae0aef62143e2daaeac012d79d1
SunshineH2 <huangshanshan@thundersdata.com>
2020-12-23 15:59:01 +0800

    fix: update comment

--- packages/react-native/src/badge/index.tsx
+++ packages/react-native/src/badge/index.tsx
@@ -16,7 +16,7 @@ type BadgeProps = BackgroundColorProps<Theme> & {
 };
 
 // 计算badge的基础数值
-const baseNum = px(24);
+const BASE_HEIGHT = px(24);
 
 const Badge: FC<BadgeProps> = ({
   type = 'text',
@@ -25,20 +25,20 @@ const Badge: FC<BadgeProps> = ({
   overflowCount = 99,
   children,
 }) => {
-  const [base, setBase] = useState(baseNum);
+  const [base, setBase] = useState(BASE_HEIGHT);
 
   useEffect(() => {
     Children.map(children, child => {
       const _child = (child as unknown) as { props: { [key: string]: string | number } };
-      const height = _child?.props.height && !Number.isNaN(+_child?.props.height) ? +_child?.props.height : baseNum;
-      const width = _child?.props.width && !Number.isNaN(+_child?.props.width) ? +_child?.props.width : baseNum;
+      const height = _child?.props.height && !Number.isNaN(+_child?.props.height) ? +_child?.props.height : BASE_HEIGHT;
+      const width = _child?.props.width && !Number.isNaN(+_child?.props.width) ? +_child?.props.width : BASE_HEIGHT;
       setBase(Math.min(width, height));
     });
   }, [base, children]);
 
   useEffect(() => {
     /** 当计算出来的base小于px(44)时，不显示ribbon，并报错 */
-    if (type === 'ribbon' && base !== baseNum && base < px(44)) {
+    if (type === 'ribbon' && base !== BASE_HEIGHT && base < px(44)) {
       throw new Error('Badge组件：请不要在children的宽高小于px(44)的情况下使用ribbon');
     }
   }, [type, base]);
