73b4f4c0b1a47654c5d5248973d4340771f4cea0
chj_damon <chjdamon@gmail.com>
2020-12-27 14:30:47 +0800

    docs: 新增Accordion组件文档

--- packages/react-native/example/src/stacks/mainStack.tsx
+++ packages/react-native/example/src/stacks/mainStack.tsx
@@ -66,7 +66,7 @@ export const MainStack = () => {
 
   return (
     <Stack.Navigator
-      initialRouteName="PullRefreshDemo"
+      initialRouteName="AccordionDemo"
       mode="card"
       headerMode="screen"
       // Stack下每个screen都会共享的配置
