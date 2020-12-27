043e29fbb8db84df63091dd73f4362abbae1db32
chj_damon <chjdamon@gmail.com>
2020-12-27 10:26:09 +0800

    refactor: 删除react-native-awesome-scrollview包

--- packages/react-native/example/src/screens/PullRefreshDemo.tsx
+++ packages/react-native/example/src/screens/PullRefreshDemo.tsx
@@ -1,29 +1,34 @@
-import React, { useMemo, useRef, useState } from 'react';
-import { PullRefresh } from '@td-design/react-native';
-import { ActivityIndicator, Text, View } from 'react-native';
+import React, { useMemo, useRef } from 'react';
+import { Text, View } from 'react-native';
 import Container from '../components/Container';
-import LottieView from 'lottie-react-native';
 import _ from 'lodash';
-import { SpringScrollView } from '@td-design/react-native-awesome-scrollview';
-
-const loadingAnimation = require('../../assets/loading.json');
+import { SpringScrollView, ChineseNormalHeader, CommonLottieHeader } from 'rn-spring-scrollview';
 
 export default () => {
-  const [progress, setProgress] = useState(0);
-  const lottieViewRef = useRef<LottieView>(null);
-
-  const handleRefresh = () => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(true);
-      }, 3000);
-    });
-  };
+  const scrollViewRef = useRef<SpringScrollView>(null);
 
   const content = useMemo(
     () => (
       <>
         <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red', backgroundColor: 'white' }}>
+          <Text>123123</Text>
+        </View>
+        <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
+          <Text>123123</Text>
+        </View>
+        <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
+          <Text>123123</Text>
+        </View>
+        <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
+          <Text>123123</Text>
+        </View>
+        <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
+          <Text>123123</Text>
+        </View>
+        <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
+          <Text>123123</Text>
+        </View>
+        <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
           <Text>123123</Text>
         </View>
         <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red' }}>
@@ -60,69 +65,16 @@ export default () => {
 
   return (
     <Container>
-      <SpringScrollView>{content}</SpringScrollView>
-      {/* FlatList + Lottie Demo */}
-      {/* <PullRefresh<{ id: number; label: string }>
-        scrollComponent="FlatList"
-        refreshComponent={
-          <LottieView
-            style={{ height: 60 }}
-            ref={lottieViewRef}
-            source={loadingAnimation}
-            progress={progress}
-            autoSize
-            loop
-          />
-        }
-        onRefresh={async () => {
-          lottieViewRef.current!.play();
-          await handleRefresh();
-          lottieViewRef.current!.reset();
+      <SpringScrollView
+        ref={scrollViewRef}
+        refreshHeader={CommonLottieHeader}
+        onRefresh={() => {
+          console.log('onRefresh');
+          setTimeout(() => scrollViewRef.current?.endRefresh(), 1000);
         }}
-        onProgress={setProgress}
-        data={Array(10)
-          .fill('')
-          .map((_, index) => ({ id: index + 1, label: `hahaha${index + 1}` }))}
-        renderItem={({ item }) => (
-          <View style={{ paddingVertical: 30, borderWidth: 1, borderColor: 'red', backgroundColor: 'white' }}>
-            <Text>{item.label}</Text>
-          </View>
-        )}
-        keyExtractor={item => item.id.toString()}
-      /> */}
-      {/* ScrollView + Lottie Demo */}
-      {/* <PullRefresh
-        refreshComponent={
-          <LottieView
-            style={{ height: 60 }}
-            ref={lottieViewRef}
-            source={loadingAnimation}
-            progress={progress}
-            autoSize
-            loop
-          />
-        }
-        onRefresh={async () => {
-          lottieViewRef.current!.play();
-          await handleRefresh();
-          lottieViewRef.current!.reset();
-        }}
-        onProgress={setProgress}
       >
         {content}
-      </PullRefresh> */}
-      {/* ScrollView + 普通组件 示例 */}
-      {/* <PullRefresh
-        refreshComponent={
-          <View style={{ height: 60, justifyContent: 'center', alignItems: 'center' }}>
-            <ActivityIndicator />
-          </View>
-        }
-        onRefresh={handleRefresh}
-        onProgress={setProgress}
-      >
-        {content}
-      </PullRefresh> */}
+      </SpringScrollView>
     </Container>
   );
 };
