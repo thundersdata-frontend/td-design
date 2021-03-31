9a35ae6f3689d5fe7a389701915dd82043aa029f
hss <2274246770@qq.com>
2021-03-31 10:26:24 +0800

    chore: 删除react-native中多余代码

--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -18,7 +18,6 @@ import Switch from './switch';
 import Input from './input';
 import ActionSheet from './action-sheet';
 import Card from './card';
-import ImagePicker from './image-picker';
 import Modal from './modal';
 import Empty from './empty';
 import Divider from './divider';
@@ -31,23 +30,18 @@ import SwipeRow from './swipe-row';
 import CollapseText from './collapse-text';
 import Toast from './toast';
 import Checkable from './checkable';
-import TapRating from './rating/TapRating';
-import SwipeRating from './rating/SwipeRating';
 import Avatar from './avatar';
 import Image from './image';
 import Slider from './slider';
 import SearchBar from './search-bar';
-import Tabs from './tabs';
 import ButtonGroup from './button-group';
 import Table from './table';
 import Swiper from './swiper';
 import ImageHeader from './image-header';
-import Calendar, { Agenda, CalendarList } from './calendar/index';
 import Flow from './flow';
 import NumberKeyboard from './number-keyboard';
 import PullRefresh from './pull-refresh';
 import Pagination from './pagination';
-import Password from './password';
 import Timeline from './time-line';
 import SvgIcon from './svg-icon';
 import CountDown from './count-down';
@@ -67,7 +61,6 @@ export {
   Accordion,
   FloatButton,
   ActionSheet,
-  ImagePicker,
   Box,
   Button,
   Flex,
@@ -100,25 +93,18 @@ export {
   CollapseText,
   Toast,
   Checkable,
-  TapRating,
-  SwipeRating,
   Avatar,
   Image,
   Slider,
   SearchBar,
-  Tabs,
   ButtonGroup,
   Table,
   Swiper,
   ImageHeader,
-  Calendar,
-  Agenda,
-  CalendarList,
   Flow,
   NumberKeyboard,
   PullRefresh,
   Pagination,
-  Password,
   Timeline,
   BoxShadow,
   SvgIcon,
