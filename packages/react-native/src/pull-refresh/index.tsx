231414524eff6ac2d3918e0c870f08ed4516dc56
rxs-michael <ruanxusong@thundersdata.com>
2021-07-01 11:51:06 +0800

    fix: 还原代码

--- packages/react-native/src/pull-refresh/index.tsx
+++ packages/react-native/src/pull-refresh/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { NativeSyntheticEvent, NativeScrollEvent, FlatList as RNFlatList } from 'react-native';
+import { NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 import { NativeViewGestureHandler, PanGestureHandler } from 'react-native-gesture-handler';
 import Animated, {
   runOnJS,
@@ -141,4 +141,4 @@ function PullRefresh({
   );
 }
 
-export default Object.assign(PullRefresh);
+export default PullRefresh;
