10278a695e40d002c29096123714de322e4899ff
hss <2274246770@qq.com>
2021-08-04 16:56:15 +0800

    feat: 新增其他-雷达图

---
+++ packages/lego/example/RadarDemo/demo1.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Radar } from '@td-design/lego';
+
+export default () => {
+  const seriesData = [
+    {
+      name: '平均情况',
+      data: [
+        { name: '占地面积', value: '51', unit: '亩' },
+        { name: '高标库面积', value: '19', unit: '万平方米' },
+        { name: '充电桩数量', value: '25', unit: '个' },
+        { name: '车辆进出数量', value: '280', unit: '辆' },
+        { name: '平均停留时长', value: '47', unit: '分钟' },
+      ],
+    },
+    {
+      name: '当前园区情况',
+      data: [
+        { name: '占地面积', value: '74.3', unit: '亩' },
+        { name: '高标库面积', value: '8.2', unit: '万平方米' },
+        { name: '充电桩数量', value: '45.1', unit: '个' },
+        { name: '车辆进出数量', value: '341.2', unit: '辆' },
+        { name: '平均停留时长', value: '56', unit: '分钟' },
+      ],
+    },
+  ];
+
+  const indicatorData = seriesData?.[0]?.data?.map((item: any) => ({
+    name: item.name,
+    max: +item.value,
+    unit: item.unit,
+  }));
+  seriesData?.[1]?.data?.forEach((item: any, index) => {
+    if (item.value > indicatorData[index].max) {
+      indicatorData[index].max = +(item.value * 1.2).toFixed(2);
+    } else {
+      indicatorData[index].max = +(indicatorData[index].max * 1.2).toFixed(2);
+    }
+  });
+
+  return <Radar style={{ width: 486, height: 354 }} seriesData={seriesData} indicatorData={indicatorData} />;
+};
--- packages/lego/src/index.tsx
+++ packages/lego/src/index.tsx
@@ -4,3 +4,4 @@ export { default as MultiLine } from './line/MultiLine';
 export { default as HorizontalBar } from './bar/HorizontalBar';
 export { default as PictorialBar } from './bar/PictorialBar';
 export { default as BarLine } from './line/BarLine';
+export { default as Radar } from './other/Radar';
