447bd0bf8dfeca68f186a6cc2801a48c64908f14
chj_damon <chjdamon@gmail.com>
2021-08-05 12:01:02 +0800

    refactor: 删除PieChart

--- packages/lego/src/index.tsx
+++ packages/lego/src/index.tsx
@@ -1,4 +1,3 @@
-export { default as PieChart } from './PieChart';
 export { default as CuboidBar } from './bar/CuboidBar';
 export { default as MultiLine } from './line/MultiLine';
 export { default as HorizontalBar } from './bar/HorizontalBar';
