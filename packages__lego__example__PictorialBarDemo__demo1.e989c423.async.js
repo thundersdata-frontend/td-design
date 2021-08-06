dff6f0ea50b9ad47558ed1dcf4fd58aa3769e27a
chj-damon <chj-damon@users.noreply.github.com>
2021-08-06 09:45:40 +0000

    deploy: d7af9715b5ff0b0c73a3134302119edc8c48d5ef

--- packages__lego__example__PictorialBarDemo__demo1.e989c423.async.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[44],{AA4d:function(t,a,e){"use strict";e.r(a);var n=e("q1tI"),i=e.n(n),s=e("qUru");a["default"]=()=>i.a.createElement(s["o"],{xAxisData:["2019\u5e74","2020\u5e74","2021\u5e74"],unit:"\u4e07",data:[2012,3630,3790],style:{width:486,height:254}})}}]);
