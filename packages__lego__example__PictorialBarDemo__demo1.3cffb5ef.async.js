96930a85d8c5919e2357ab7565cd8ec9cef9ff46
chj-damon <chj-damon@users.noreply.github.com>
2021-08-06 03:53:50 +0000

    deploy: 87e94f9110c7fb2b039d39cb4a22e5f874ee512c

---
+++ packages__lego__example__PictorialBarDemo__demo1.3cffb5ef.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[42],{AA4d:function(t,a,e){"use strict";e.r(a);var n=e("q1tI"),i=e.n(n),s=e("qUru");a["default"]=()=>i.a.createElement(s["m"],{xAxisData:["2019\u5e74","2020\u5e74","2021\u5e74"],unit:"\u4e07",data:[2012,3630,3790],style:{width:486,height:254}})}}]);
---
+++ packages__lego__example__PictorialBarDemo__demo2.cfd41f0a.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[43],{U65p:function(t,a,e){"use strict";e.r(a);var n=e("q1tI"),i=e.n(n),s=e("qUru");a["default"]=()=>i.a.createElement(s["m"],{xAxisData:["2009\u5e74","2010\u5e74","2011\u5e74","2012\u5e74","2013\u5e74","2014\u5e74"],unit:"\u4e07",data:[2012,3630,3790,1900,4560,7300],style:{width:486,height:254}})}}]);
