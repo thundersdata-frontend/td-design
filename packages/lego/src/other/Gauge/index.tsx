14680dd911060c7c0dc42abaecb2c11c78343541
陈英杰 <13487079308@163.com>
2021-08-11 10:39:34 +0800

    chore: 修改颜色样式

--- packages/lego/src/other/Gauge/index.tsx
+++ packages/lego/src/other/Gauge/index.tsx
@@ -1,11 +1,12 @@
-import React, { CSSProperties, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-
-import theme from '../../theme';
+import React, { CSSProperties, useCallback, useEffect, useMemo, useRef } from 'react';
+import color from 'color';
+import useTheme from '../../hooks/useTheme';
 
 /**
  * 仪表盘图
  */
 export default ({ value = 62, max = 100, style = {} }: { max: number; value: number; style?: CSSProperties }) => {
+  const theme = useTheme();
   // 当前的值,保存有动画
   const valueRef = useRef(0);
 
@@ -24,12 +25,17 @@ export default ({ value = 62, max = 100, style = {} }: { max: number; value: num
   const lineNums = 120;
   const innerLineNums = 180;
   const colorArr = useMemo(() => {
-    return ['#FF50B3', '#FFED44', '#3DE6FF', '#1968FF'];
-  }, []);
+    return [
+      theme.colors.primary400[0],
+      theme.colors.primary300[0],
+      theme.colors.primary200[0],
+      theme.colors.primary100[0],
+    ];
+  }, [theme.colors.primary100, theme.colors.primary200, theme.colors.primary300, theme.colors.primary400]);
 
   const lineColor = useMemo(() => {
-    return ['#FF1866', '#21E2FF', '#0A5EFF'];
-  }, []);
+    return [theme.colors.primary400[0], theme.colors.primary50[1], theme.colors.primary100[0]];
+  }, [theme.colors.primary100, theme.colors.primary400, theme.colors.primary50]);
 
   const numberColors = useMemo(() => {
     return [
@@ -40,99 +46,46 @@ export default ({ value = 62, max = 100, style = {} }: { max: number; value: num
       theme.colors.primary50,
       theme.colors.primary100,
     ];
-  }, []);
+  }, [
+    theme.colors.primary100,
+    theme.colors.primary200,
+    theme.colors.primary300,
+    theme.colors.primary400,
+    theme.colors.primary50,
+  ]);
 
-  const colorRgb = useCallback((color: string) => {
-    const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
-    let sColor = color.toLowerCase();
-    if (sColor && reg.test(sColor)) {
-      if (sColor.length === 4) {
-        let sColorNew = '#';
-        for (let i = 1; i < 4; i += 1) {
-          sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
-        }
-        sColor = sColorNew;
-      }
-      //处理六位的颜色值
-      const sColorChange = [];
-      for (let i = 1; i < 7; i += 2) {
-        sColorChange.push(parseInt('0x' + sColor.slice(i, i + 2)));
-      }
-      return sColorChange;
-    } else {
-      return sColor;
+  const gradientColor = useCallback((startColor, endColor, step) => {
+    const startRGB = color.rgb(startColor).array(); //转换为rgb数组模式
+    const startR = startRGB[0];
+    const startG = startRGB[1];
+    const startB = startRGB[2];
+
+    const endRGB = color.rgb(endColor).array();
+    const endR = endRGB[0];
+    const endG = endRGB[1];
+    const endB = endRGB[2];
+
+    const sR = (+endR - +startR) / step; //总差值
+    const sG = (+endG - +startG) / step;
+    const sB = (+endB - +startB) / step;
+
+    const colorArr = [];
+
+    for (let i = 0; i < step; i++) {
+      //计算每一步的hex值
+      const hex = color.hsl(
+        'rgb(' +
+          parseInt(+sR * i + +startR + '') +
+          ',' +
+          parseInt(+sG * i + +startG + '') +
+          ',' +
+          parseInt(+sB * i + +startB + '') +
+          ')'
+      );
+      colorArr.push(hex);
     }
+    return colorArr;
   }, []);
-
-  const colorHex = useCallback(rgb => {
-    const _this = rgb;
-    const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
-    if (/^(rgb|RGB)/.test(_this)) {
-      const aColor = _this.replace(/(?:(|)|rgb|RGB)*/g, '').split(',');
-      let strHex = '#';
-      for (let i = 0; i < aColor.length; i++) {
-        let hex = Number(aColor[i]).toString(16);
-        hex = +hex < 10 ? 0 + '' + hex : hex; // 保证每个rgb的值为2位
-        if (hex === '0') {
-          hex += hex;
-        }
-        strHex += hex;
-      }
-      if (strHex.length !== 7) {
-        strHex = _this;
-      }
-      return strHex;
-    } else if (reg.test(_this)) {
-      const aNum = _this.replace(/#/, '').split('');
-      if (aNum.length === 6) {
-        return _this;
-      } else if (aNum.length === 3) {
-        let numHex = '#';
-        for (let i = 0; i < aNum.length; i += 1) {
-          numHex += aNum[i] + aNum[i];
-        }
-        return numHex;
-      }
-    } else {
-      return _this;
-    }
-  }, []);
-
-  const gradientColor = useCallback(
-    (startColor, endColor, step) => {
-      const startRGB = colorRgb(startColor); //转换为rgb数组模式
-      const startR = startRGB[0];
-      const startG = startRGB[1];
-      const startB = startRGB[2];
-
-      const endRGB = colorRgb(endColor);
-      const endR = endRGB[0];
-      const endG = endRGB[1];
-      const endB = endRGB[2];
-
-      const sR = (+endR - +startR) / step; //总差值
-      const sG = (+endG - +startG) / step;
-      const sB = (+endB - +startB) / step;
-
-      const colorArr = [];
-
-      for (let i = 0; i < step; i++) {
-        //计算每一步的hex值
-        const hex = colorHex(
-          'rgb(' +
-            parseInt(+sR * i + +startR + '') +
-            ',' +
-            parseInt(+sG * i + +startG + '') +
-            ',' +
-            parseInt(+sB * i + +startB + '') +
-            ')'
-        );
-        colorArr.push(hex);
-      }
-      return colorArr;
-    },
-    [colorHex, colorRgb]
-  );
 
   const colorList = useMemo(() => {
     let list: any[] = [];
@@ -270,17 +223,20 @@ export default ({ value = 62, max = 100, style = {} }: { max: number; value: num
     ctx.restore();
   }, []);
 
-  const drawValue = useCallback(value => {
-    const ctx = canvasRef.current?.getContext('2d');
-    if (!ctx) return;
-    ctx.save();
-    ctx.rotate((210 / 180) * Math.PI);
-    ctx.fillStyle = theme.colors.gray50;
-    ctx.font = '72px Roboto';
-    ctx.textAlign = 'center';
-    ctx.fillText(value + '', 0, 120);
-    ctx.restore();
-  }, []);
+  const drawValue = useCallback(
+    value => {
+      const ctx = canvasRef.current?.getContext('2d');
+      if (!ctx) return;
+      ctx.save();
+      ctx.rotate((210 / 180) * Math.PI);
+      ctx.fillStyle = theme.colors.gray50;
+      ctx.font = '72px Roboto';
+      ctx.textAlign = 'center';
+      ctx.fillText(value + '', 0, 120);
+      ctx.restore();
+    },
+    [theme.colors.gray50]
+  );
 
   const init = useCallback(() => {
     if (valueRef.current !== value) {
