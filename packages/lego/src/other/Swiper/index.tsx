1de7d57cc2bd164cf1ef72d5e7131a4fc4522e72
hss <2274246770@qq.com>
2021-08-12 14:46:06 +0800

    feat: 增加轮播图组件hover时停止滚动的效果以及补充例子

--- packages/lego/src/other/Swiper/index.tsx
+++ packages/lego/src/other/Swiper/index.tsx
@@ -1,16 +1,15 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, ReactNode } from 'react';
 import SwiperCore, { Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper.less';
 import 'swiper/components/pagination/pagination.less';
 import './index.less';
-import { ReactNode } from 'react';
 
 SwiperCore.use([Pagination, Autoplay]);
 
 type CustomSwiperProps = {
   /** 需要轮播的图片 */
-  imgs: string[];
+  imgs?: string[];
   /** img的样式，主要为宽度和高度 */
   style?: React.CSSProperties;
   /** 是否自动播放，delay为切换速度(ms) */
@@ -25,6 +24,8 @@ type CustomSwiperProps = {
 
 const CustomSwiper = forwardRef<any, CustomSwiperProps>(
   ({ imgs = [], style, imgNumPerSlide = 1, autoplay, list = [] }, ref) => {
+    const auto = autoplay?.delay ? { pauseOnMouseEnter: true, disableOnInteraction: false, ...autoplay } : false;
+
     return (
       <div className="td-lego-swiper-container">
         {imgs.length > 0 || list.length > 0 ? (
@@ -34,7 +35,7 @@ const CustomSwiper = forwardRef<any, CustomSwiperProps>(
             slidesPerGroup={imgNumPerSlide}
             loop
             pagination={{ clickable: true }}
-            autoplay={autoplay}
+            autoplay={auto}
             ref={ref}
             initialSlide={0}
           >
