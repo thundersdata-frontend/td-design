f94267106d10fe33cb0abd4615c34bd2ef724e0c
chj_damon <chjdamon@gmail.com>
2021-09-18 16:07:26 +0800

    feat: 支持eslint fix命令自动修复

--- packages/eslint-plugin-replace-hooks/README.md
+++ packages/eslint-plugin-replace-hooks/README.md
@@ -22,25 +22,32 @@ Add `replace-hooks` to the plugins section of your `.eslintrc` configuration fil
 
 ```json
 {
-    "plugins": [
-        "replace-hooks"
-    ]
+  "plugins": [
+    "replace-hooks" // this must be placed before `import`
+  ]
 }
 ```
-
 
 Then configure the rules you want to use under the rules section.
 
 ```json
 {
-    "rules": {
-        "replace-hooks/rule-name": 2
-    }
+  "rules": {
+    "replace-hooks/no-forbidden-hooks": [
+      "error",
+      { "useState": { "tip": "your custom hooks", "dependency": "your module" } }
+    ]
+  }
 }
 ```
 
 ## Supported Rules
 
-* Fill in provided rules here
+- no-forbidden-hooks
 
+## Caution
 
+- this plugin should work with `eslint-plugin-import`, especially the rules `import/no-duplicates` to help merge import from same modules.
+- the plugin must be placed before `eslint-plugin-import`
+
+  plugins: [ // other plugins, 'replace-hooks', 'import', ]
--- packages/eslint-plugin-replace-hooks/src/rules/no-forbidden-hooks.js
+++ packages/eslint-plugin-replace-hooks/src/rules/no-forbidden-hooks.js
@@ -2,7 +2,7 @@
  * @fileoverview no forbidden hooks in project
  * @author chjdamon
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Rule Definition
@@ -10,27 +10,28 @@
 
 module.exports = {
   meta: {
-    type: 'problem', // `problem`, `suggestion`, or `layout`
+    type: 'suggestion', // `problem`, `suggestion`, or `layout`
     docs: {
-      description: "no forbidden hooks in project",
-      category: "Fill me in",
-      recommended: false,
-      url: null, // URL to the documentation page for this rule
+      description: 'no forbidden hooks in project',
+      recommended: true,
+      url: 'https://github.com/thundersdata-frontend/td-design/tree/main/packages/eslint-plugin-replace-hooks#readme', // URL to the documentation page for this rule
     },
-    fixable: null, // Or `code` or `whitespace`
-    schema: [{
-      type: 'object'
-    }], // Add a schema if the rule has options
+    fixable: 'code', // Or `code` or `whitespace`
+    schema: [
+      {
+        type: 'object',
+      },
+    ], // Add a schema if the rule has options
     messages: {
-      forbidden: "`{{name}}` is forbidden. try to use `{{replace}}` alternatively"
-    }
+      empty: '',
+      forbidden: '`{{name}}` is forbidden. try to use `{{tip}}` from `{{dependency}}` alternatively',
+    },
   },
 
   create(context) {
     // variables should be defined here
     if (!context.options || !Array.isArray(context.options) || context.options.length === 0) return {};
-    const pattern = context.options[0];
-
+    const pattern = context.options[0]; // {useState: {tip: 'useSafeState', dependency: '@td-design/rn-hooks'}}
     //----------------------------------------------------------------------
     // Helpers
     //----------------------------------------------------------------------
@@ -42,8 +43,8 @@ module.exports = {
     //----------------------------------------------------------------------
     if (!pattern) return {};
     return {
-      // visitor functions for different types of nodes
-      'ImportDeclaration ImportSpecifier': node => {
+      // 删除要替换的hooks，插入代替的hooks的import
+      ImportSpecifier: node => {
         Object.entries(pattern).forEach(([key, value]) => {
           if (node.imported.name === key) {
             context.report({
@@ -51,12 +52,49 @@ module.exports = {
               messageId: 'forbidden',
               data: {
                 name: key,
-                replace: value,
-              }
+                tip: value.tip,
+                dependency: value.dependency,
+              },
+              fix: fixer => [
+                fixer.remove(node),
+                fixer.insertTextAfterRange(
+                  [node.parent.range[0], node.parent.range[1] + 1],
+                  `import { ${value.tip} } from '${value.dependency}';\n`
+                ),
+              ],
             });
           }
-        })
-      }
+        });
+      },
+
+      // 删除空的import
+      ImportDeclaration: node => {
+        if (node.specifiers.length === 0) {
+          context.report({
+            node,
+            messageId: 'empty',
+            fix: fixer => fixer.remove(node),
+          });
+        }
+      },
+
+      // 替换文本
+      'VariableDeclaration VariableDeclarator CallExpression Identifier': node => {
+        Object.entries(pattern).forEach(([key, value]) => {
+          if (node.name === key) {
+            context.report({
+              node,
+              messageId: 'forbidden',
+              data: {
+                name: key,
+                tip: value.tip,
+                dependency: value.dependency,
+              },
+              fix: fixer => fixer.replaceText(node, value.tip),
+            });
+          }
+        });
+      },
     };
   },
 };
