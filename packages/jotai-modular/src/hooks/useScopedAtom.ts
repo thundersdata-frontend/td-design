41126681fbae95215578a6885706cb9e6dd1475a
chj_damon <chjdamon@gmail.com>
2021-09-30 12:05:11 +0800

    fix: 修复jotai-modular报错提示atom名字不正确的问题

--- packages/jotai-modular/src/hooks/useScopedAtom.ts
+++ packages/jotai-modular/src/hooks/useScopedAtom.ts
@@ -9,7 +9,7 @@ export default function useScopedAtom<T, U>(atom: WritableAtom<T, U>) {
   const [value, setValue] = useAtom(atom, scope);
 
   if (typeof value === 'symbol' && scope && value.toString() !== scope.toString()) {
-    throw new Error(`${atom.toString()} cannot be used in ${getModuleName(scope.toString())}`);
+    throw new Error(`${atom.debugLabel} cannot be used in ${getModuleName(scope.toString())}`);
   }
-  return [value, setValue] as const;
+  return [value, setValue];
 }
--- packages/jotai-modular/src/hooks/useScopedAtomValue.ts
+++ packages/jotai-modular/src/hooks/useScopedAtomValue.ts
@@ -9,7 +9,7 @@ export default function useScopedAtomValue<T, U>(atom: WritableAtom<T, U>) {
   const value = useAtomValue(atom, scope);
 
   if (typeof value === 'symbol' && scope && value.toString() !== scope.toString()) {
-    throw new Error(`${atom.toString()} cannot be used in ${getModuleName(scope.toString())}`);
+    throw new Error(`${atom.debugLabel} cannot be used in ${getModuleName(scope.toString())}`);
   }
   return value;
 }
