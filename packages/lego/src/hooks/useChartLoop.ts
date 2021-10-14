a251e2e1191f183598c6679c623812f191284c63
rxs-michael <ruanxusong@thundersdata.com>
2021-10-14 10:45:48 +0800

    fix: 修复图表 ref 传入函数无法自动轮播 bug

--- packages/lego/src/hooks/useChartLoop.ts
+++ packages/lego/src/hooks/useChartLoop.ts
@@ -15,9 +15,11 @@ export default function useChartLoop(
   // 图表实例，轮播的本质是用 currentIndex 来驱动图表实例去 dispatchAction
   const _echartsRef = useRef<ReactEcharts>(null);
 
-  const echartsRef = ref ? (ref as MutableRefObject<ReactEcharts>) : _echartsRef;
+  const echartsRef = ref
+    ? (ref as MutableRefObject<ReactEcharts> | ((ref: ReactEcharts | null) => ECharts))
+    : _echartsRef;
 
-  const instance = echartsRef?.current?.getEchartsInstance() as ECharts;
+  const [instance, setInstance] = useState<ECharts>();
 
   const { raf } = useRAF();
   const timer = useRef<symbol>();
@@ -26,9 +28,16 @@ export default function useChartLoop(
   const current = data[currentIndex];
   const currentName = typeof current === 'string' ? current : current?.name;
 
+  /** 初始设置图表实例 */
+  useEffect(() => {
+    if (typeof echartsRef !== 'function') {
+      setInstance(echartsRef?.current?.getEchartsInstance());
+    }
+  }, [echartsRef]);
+
   useEffect(() => {
     // 开启自动轮播，同时echarts有示例，同时有数据的情况下，才开始
-    if (autoLoop && echartsRef.current && length > 1) {
+    if (autoLoop && (typeof echartsRef === 'function' || echartsRef.current) && length > 1) {
       timer.current = raf.setInterval(() => {
         setCurrentIndex(index => (index >= length - 1 ? 0 : index + 1));
       }, duration);
@@ -43,30 +52,43 @@ export default function useChartLoop(
 
   // 用 currentIndex 来驱动图表变化
   useEffect(() => {
+    if (!instance) {
+      return;
+    }
     // 取消高亮效果
-    instance?.dispatchAction({
+    instance.dispatchAction({
       type: 'downplay',
     });
     // 先把提示框都隐藏
-    instance?.dispatchAction({
+    instance.dispatchAction({
       type: 'hideTip',
     });
 
     if (currentIndex > -1) {
       // 再根据 currentIndex 显示对应的提示框
-      instance?.dispatchAction({
+      instance.dispatchAction({
         type: 'showTip',
         seriesIndex,
         dataIndex: currentIndex,
       });
 
       currentName &&
-        instance?.dispatchAction({
+        instance.dispatchAction({
           type: 'highlight',
           name: currentName,
         });
     }
   }, [currentIndex, currentName, instance, seriesIndex]);
 
-  return echartsRef;
+  return typeof echartsRef === 'function'
+    ? (ref: ReactEcharts) => {
+        // 执行传入的 ref 函数
+        echartsRef(ref);
+        if (!ref) {
+          return;
+        }
+        // 设置当前图表实例
+        setInstance(ref.getEchartsInstance());
+      }
+    : echartsRef;
 }
