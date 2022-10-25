ba9fb6111f112785b381bf4bb1185cb89ddfa6e7
chj_damon <chjdamon@gmail.com>
2022-10-25 11:02:11 +0800

    feat: 新增react-native-echarts包

---
+++ packages/react-native-echarts/src/utils/builder.ts
@@ -0,0 +1,100 @@
+export const formatString = (str?: string): string => {
+  if (!str) return '';
+  return str
+    .replace(/(\w+:)|(\w+ :)/g, s => {
+      return `"${s.substring(0, s.length - 1)}":`;
+    })
+    .replace(/\\"/g, "'")
+    .replace(/"function (\w+)\((.*)\) ?\{([\n\s\S]*?)\}"/g, (_, a, b, c) => {
+      return `function ${a}(${b}){${formatString(c)}}`;
+    });
+};
+
+export type EchartsInitOptions = {
+  devicePixelRatio?: number;
+  renderer?: 'canvas' | 'svg';
+  locale?: 'ZH' | 'EN';
+  useDirtyRect?: boolean;
+};
+
+export const buildEcharts = (backgroundColor: string, echartsInitOptions: EchartsInitOptions) => {
+  return `
+    function setBackgroundColor(color) {
+      document.getElementById('main').style.backgroundColor = color;
+    }
+
+    function sendCallbackData(uuid, data) {
+      window.ReactNativeWebView.postMessage(JSON.stringify({"types":"CALLBACK", "uuid": uuid, "payload": data}));
+    }
+
+    function parse (data) {
+      return JSON.parse(data, function (key, value) {
+        if (value
+          && typeof value === "string"
+          && value.substr(0,8) === "function"
+        ) {
+          var startBody = value.indexOf('{') + 1;
+          var endBody = value.lastIndexOf('}');
+          var startArgs = value.indexOf('(') + 1;
+          var endArgs = value.indexOf(')');
+
+          return new Function(value.substring(startArgs, endArgs), value.substring(startBody, endBody));
+        }
+        return value;
+      });
+    }
+
+    window.onresize = function() {
+      chart.resize();
+    };
+
+    function processMessage(e) {
+      var req = parse(e.data);
+
+      switch(req.types) {
+        case "SET_OPTION":
+          chart.setOption(eval('(req.payload.option)'), req.payload.notMerge, req.payload.lazyUpdate);
+          break;
+
+        case "CLEAR":
+          chart.clear();
+          break;
+
+        case "SET_BACKGROUND_COLOR":
+          setBackgroundColor(req.color);
+          break;
+
+        case "GET_OPTION":
+          var option = chart.getOption();
+          var data = {};
+
+          if(req.properties !== undefined) {
+            req.properties.forEach(function (prop) {
+              data[prop] = option[prop];
+            });
+          } else {
+            var data = {
+              option: option
+            };
+          }
+          sendCallbackData(req.uuid, data);
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    window.document.addEventListener('message', function(e) {
+      processMessage(e);
+    });
+
+    window.addEventListener('message', function(e) {
+      processMessage(e);
+    });
+
+    var chart = echarts.init(document.getElementById('main'), undefined, ${JSON.stringify(echartsInitOptions)});
+    setBackgroundColor("${backgroundColor}");
+    true;
+  `;
+};
