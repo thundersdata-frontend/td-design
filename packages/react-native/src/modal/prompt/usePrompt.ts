2be946e19340efaec084ff99ac72e42673daf4e3
陈英杰 <13487079308@163.com>
2023-05-17 17:54:19 +0800

    refactor(custom): 修改一些写法

--- packages/react-native/src/modal/prompt/usePrompt.ts
+++ packages/react-native/src/modal/prompt/usePrompt.ts
@@ -1,6 +1,4 @@
-import { useCallback } from 'react';
-
-import { useSafeState } from '@td-design/rn-hooks';
+import { useMemoizedFn, useSafeState } from '@td-design/rn-hooks';
 
 import useConfirm from '../confirm/useConfirm';
 import { PromptProps } from '../type';
@@ -8,13 +6,10 @@ import { PromptProps } from '../type';
 export default function usePrompt({ onOk, onCancel }: Pick<PromptProps, 'onOk' | 'onCancel'>) {
   const [value, onChange] = useSafeState();
 
-  const okFun = useCallback(() => {
-    const result = onOk?.(value);
-    if (result?.then) {
-      return Promise.resolve(result);
-    }
+  const okFun = useMemoizedFn(async () => {
+    const result = await onOk?.(value);
     return result;
-  }, [onOk, value]);
+  });
 
   const { visible, okBtnLoading, cancelBtnLoading, hide, handleOk, handleCancel } = useConfirm({
     onOk: okFun,
