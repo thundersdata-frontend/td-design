12b6da51d791a2008c47b88bd44fa8a96c9c1b91
陈英杰 <13487079308@163.com>
2023-05-17 16:31:36 +0800

    fix(custom): 修改prompt回调没有参数的问题

--- packages/react-native/src/modal/prompt/usePrompt.ts
+++ packages/react-native/src/modal/prompt/usePrompt.ts
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 import { useSafeState } from '@td-design/rn-hooks';
 
 import useConfirm from '../confirm/useConfirm';
@@ -6,7 +8,18 @@ import { PromptProps } from '../type';
 export default function usePrompt({ onOk, onCancel }: Pick<PromptProps, 'onOk' | 'onCancel'>) {
   const [value, onChange] = useSafeState();
 
-  const { visible, okBtnLoading, cancelBtnLoading, hide, handleOk, handleCancel } = useConfirm({ onOk, onCancel });
+  const okFun = useCallback(() => {
+    const result = onOk?.(value);
+    if (result?.then) {
+      return Promise.resolve(result);
+    }
+    return result;
+  }, [onOk, value]);
+
+  const { visible, okBtnLoading, cancelBtnLoading, hide, handleOk, handleCancel } = useConfirm({
+    onOk: okFun,
+    onCancel,
+  });
 
   return {
     value,
