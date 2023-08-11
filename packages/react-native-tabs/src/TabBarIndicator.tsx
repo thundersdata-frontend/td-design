150819de77bac843b33023be6bc2a7955d08a01b
陈杰 <chj_0507_dz@sina.com>
2023-08-11 09:52:31 +0800

    fix: 优化指示器的动画效果

--- packages/react-native-tabs/src/TabBarIndicator.tsx
+++ packages/react-native-tabs/src/TabBarIndicator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import Animated, { interpolate, useAnimatedStyle } from 'react-native-reanimated';
+import Animated, { Extrapolate, interpolate, useAnimatedStyle } from 'react-native-reanimated';
 
 import { TabBarIndicatorProps } from './type';
 
@@ -18,8 +18,8 @@ export default function TabBarIndicator({ style, scrollX, inputRange, scrollRang
   });
 
   const animatedStyles = useAnimatedStyle(() => {
-    const translateX = interpolate(scrollX.value, inputRange, scrollRange);
-    const width = interpolate(scrollX.value, inputRange, tabWidths);
+    const translateX = interpolate(scrollX.value, inputRange, scrollRange, Extrapolate.CLAMP);
+    const width = interpolate(scrollX.value, inputRange, tabWidths, Extrapolate.CLAMP);
 
     return {
       width,
