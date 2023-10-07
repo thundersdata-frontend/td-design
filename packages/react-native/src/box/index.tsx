d29c3c4fdd6ede126a1e95a266d25b41d0e784b0
陈杰 <chj_0507_dz@sina.com>
2023-10-07 11:39:00 +0800

    perf: 对Box和Text两个基础组件进行性能优化

--- packages/react-native/src/box/index.tsx
+++ packages/react-native/src/box/index.tsx
@@ -1,8 +1,11 @@
+// @ts-ignore
+import NativeView from 'react-native/Libraries/Components/View/ViewNativeComponent';
+
 import { createBox } from '@shopify/restyle';
 
 import { Theme } from '../theme';
 
-const Box = createBox<Theme>();
+const Box = createBox<Theme>(NativeView);
 Box.displayName = 'Box';
 Box.defaultProps = {
   pointerEvents: 'box-none',
