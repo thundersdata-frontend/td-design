da60d29ed5151d57bfd07cc82f7aebd5c5fd9863
陈杰 <chj_0507_dz@sina.com>
2024-09-02 17:19:47 +0800

    feat: 为Modal增加show方法

---
+++ .changeset/giant-tables-hunt.md
@@ -0,0 +1,5 @@
+---
+'@td-design/react-native': patch
+---
+
+feat: 为Modal增加show方法，实现指令式打开弹窗的功能
--- packages/react-native/src/modal/index.tsx
+++ packages/react-native/src/modal/index.tsx
@@ -2,6 +2,7 @@ import alert from './alert';
 import confirm from './confirm';
 import Modal from './Modal';
 import prompt from './prompt';
+import show from './show';
 import tip from './tip';
 
-export default Object.assign(Modal, { alert, confirm, prompt, tip });
+export default Object.assign(Modal, { alert, confirm, prompt, tip, show });
